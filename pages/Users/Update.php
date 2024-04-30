fe01ec41f814d87eafe2e1b40e19e2c7f81f9912
Mukund-Kanzariya <mukundkanzariya4@gmail.com>
2024-04-30 10:31:00 +0530

    Users pages

---
+++ pages/Users/Update.php
@@ -0,0 +1,89 @@
+<?php
+
+require('../../includes/init.php');
+include pathOf('includes/header.php');
+include pathOf('includes/navbar.php');
+
+?>
+
+	<!--wrapper-->
+
+	<div class="page-wrapper">
+			<div class="page-content">
+		<div class="section-authentication-cover">
+			<div class="">
+				<div class="row g-0">
+
+					<div class="col-12 col-xl-7 col-xxl-8 auth-cover-left align-items-center justify-content-center d-none d-xl-flex">
+
+                        <div class="card shadow-none bg-transparent shadow-none rounded-0 mb-0">
+							<div class="card-body">
+                                 <img src="<?= urlOf('assets/images/login-images/user.jpg') ?>" class="img-fluid auth-img-cover-login" width="1000" alt=""/>
+							</div>
+						</div>
+						
+					</div>
+
+					<div class="col-12 col-xl-5 col-xxl-4 auth-cover-right bg-light align-items-center justify-content-center">
+						<div class="card rounded-0 m-3 shadow-none bg-transparent mb-0">
+							<div class="card-body p-sm-5">
+								<div class="">
+									<div class="mb-3 text-center">
+										<img src="<?= urlOf('assets/images/logo-icon.png') ?>" width="60" alt="">
+									</div>
+									<div class="text-center mb-4">
+										<h5 class="">Update User</h5>
+									</div>
+									<div class="form-body">
+										<form class="row g-3">
+											<div class="col-12">
+												<label for="inputEmailAddress" class="form-label">Sr.No</label>
+												<input type="text" class="form-control" id="id" >
+											</div>
+                                            <div class="col-12">
+												<label for="inputEmailAddress" class="form-label">RoleName</label>
+												<input type="text" class="form-control" id="roleid" >
+											</div>
+											<div class="col-12">
+												<label for="inputChoosePassword" class="form-label">Name</label>
+													<input type="text" class="form-control border-end-0" id="name"  >
+											</div>
+                                            <div class="col-12">
+												<label for="inputChoosePassword" class="form-label">Mobile No</label>
+													<input type="text" class="form-control border-end-0" id="mobile" >
+											</div>
+                                            <div class="col-12">
+												<label for="inputChoosePassword" class="form-label">Salary</label>
+													<input type="text" class="form-control border-end-0" id="salary" >
+											</div>
+                                            <div class="col-12">
+												<label for="inputChoosePassword" class="form-label">E-mail</label>
+													<input type="text" class="form-control border-end-0" id="email" >
+											</div>
+											
+											
+											<div class="col-12">
+												<div class="d-grid">
+													<button type="submit" class="btn btn-light">Update</button>
+												</div>
+											</div>
+											
+										</form>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php
+
+include pathOf('includes/footer.php');
+include pathOf('includes/scripts.php');
+include pathOf('includes/pageEnd.php');
+
+?>
---
+++ pages/Users/index.php
@@ -0,0 +1,230 @@
+<?php
+
+require ('../../includes/init.php');
+include pathOf('includes/header.php');
+include pathOf('includes/navbar.php');
+
+?>
+
+		   <!--end header wrapper-->
+		<!--start page wrapper -->
+		<div class="page-wrapper">
+			<div class="page-content">
+				<!--breadcrumb-->
+				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+					<div class="breadcrumb-title pe-3">Tables</div>
+					<div class="ps-3">
+						<nav aria-label="breadcrumb">
+							<ol class="breadcrumb mb-0 p-0">
+								<li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+								</li>
+								<li class="breadcrumb-item active" aria-current="page">Data Table</li>
+							</ol>
+						</nav>
+					</div>
+					
+				</div>
+				<!--end breadcrumb-->
+				<h6 class="mb-0 text-uppercase">USERS DATA</h6>
+				<hr/>
+				<div class="card">
+					<div class="card-body">
+						<div class="table-responsive">
+							<table id="example" class="table table-striped table-bordered" style="width:100%">
+								<thead>
+									<tr>
+										<th>Sr.No</th>
+										<th>RoleName</th>
+										<th>Name</th>
+										<th>Mobile</th>
+										<th>Salary</th>
+										<th>E-mail</th>
+										<th>Modify</th>
+										<th>Delete</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+									<tr>
+										<td>Tiger Nixon</td>
+										<td>System Architect</td>
+										<td>Edinburgh</td>
+										<td>61</td>
+										<td>2011/04/25</td>
+										<td>$320,800</td>
+										<td><a href="#" class="btn btn-primary active" aria-current="page">Update</a></td>
+										<td><a href="#" class="btn btn-danger active" aria-current="page">Delete</a></td>
+									</tr>
+								<tfoot>
+								<tr>
+										<th>Sr.No</th>
+										<th>RoleName</th>
+										<th>Name</th>
+										<th>Mobile</th>
+										<th>Salary</th>
+										<th>E-mail</th>
+										<th>Modify</th>
+										<th>Delete</th>
+									</tr>
+								</tfoot>
+							</table>
+						</div>
+					</div>
+				</div>
+	
+<?php
+
+include pathOf('includes/footer.php');
+include pathOf('includes/scripts.php');
+include pathOf('includes/pageEnd.php');
+
+?>
