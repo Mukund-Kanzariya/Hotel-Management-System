24ef6b027735a285ee8700c573e200501783eb57
Mukund-Kanzariya <mukundkanzariya4@gmail.com>
2024-05-08 11:12:06 +0530

    login page

---
+++ pages/Login/index.php
@@ -0,0 +1,257 @@
+<?php
+
+require ('../../includes/init.php');
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+
+<!-- Mirrored from codervent.com/dashtrans/demo/horizontal/auth-basic-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 11 Apr 2024 10:15:31 GMT -->
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--favicon-->
+	<link rel="icon" href="<?= urlOf('assets/images/favicon-32x32.png')?>" type="image/png" />
+	<!--plugins-->
+	<link href="<?= urlOf('assets/plugins/simplebar/css/simplebar.css') ?>" rel="stylesheet" />
+	<link href="<?= urlOf('assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') ?>" rel="stylesheet" />
+	<link href="<?= urlOf('assets/plugins/metismenu/css/metisMenu.min.css') ?>" rel="stylesheet" />
+	<!-- loader-->
+	<link href="<?= urlOf('assets/css/pace.min.css') ?>" rel="stylesheet" />
+	<script src="<?= urlOf('assets/js/pace.min.js') ?>"></script>
+	<!-- Bootstrap CSS -->
+	<link href="<?= urlOf('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+	<link href="<?= urlOf('assets/css/bootstrap-extended.css') ?>" rel="stylesheet">
+	<link href="<?= urlOf('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap') ?>" rel="stylesheet">
+	<link href="<?= urlOf('assets/css/app.css') ?>" rel="stylesheet">
+	<link href="<?= urlOf('assets/css/icons.css') ?>" rel="stylesheet">
+	<title>Hotel-Management-System</title>
+</head>
+
+<body class="bg-theme bg-theme2">
+	<!--wrapper-->
+	<div class="wrapper">
+		<div class="section-authentication-signin d-flex align-items-center justify-content-center my-5 my-lg-0">
+			<div class="container">
+				<div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
+					<div class="col mx-auto">
+						<div class="card mb-0">
+							<div class="card-body">
+								<div class="p-4">
+									<div class="mb-3 text-center">
+										<img src="<?= urlOf('assets/images/logo-icon.png') ?>" width="60" alt="" />
+									</div>
+									<div class="text-center mb-4">
+										<h5 class="">StayComfirt</h5>
+										<p class="mb-0">Please log in to your account</p>
+									</div>
+									<div class="form-body">
+										<form class="row g-3">
+											<div class="col-12">
+												<label for="username" class="form-label">username</label>
+												<input type="text" class="form-control" id="username" placeholder="Enter UserName">
+											</div>
+											<div class="col-12">
+												<label for="inputChoosePassword" class="form-label">Password</label>
+												<div class="input-group" id="show_hide_password">
+													<input type="password" class="form-control border-end-0" id="password"  placeholder="Enter Password"> <a href="javascript:;" class="input-group-text bg-transparent"><i class='bx bx-hide'></i></a>
+												</div>
+											</div>
+											<div class="col-md-6">
+												<div class="form-check form-switch">
+												</div>
+											</div>
+											</div>
+											<div class="col-12">
+												<div class="d-grid">
+													<button type="button" class="btn btn-light" onclick="sendData()">Sign in</button>
+												</div>
+											</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!--end row-->
+			</div>
+		</div>
+	</div>
+	<!--end wrapper-->
+	<!--start switcher-->
+	<div class="switcher-wrapper">
+		<div class="switcher-btn"> <i class='bx bx-cog bx-spin'></i>
+		</div>
+		<div class="switcher-body">
+			<div class="d-flex align-items-center">
+				<h5 class="mb-0 text-uppercase">Theme Customizer</h5>
+				<button type="button" class="btn-close ms-auto close-switcher" aria-label="Close"></button>
+			</div>
+			<hr/>
+			<p class="mb-0">Gaussian Texture</p>
+			  <hr>
+			  
+			  <ul class="switcher">
+				<li id="theme1"></li>
+				<li id="theme2"></li>
+				<li id="theme3"></li>
+				<li id="theme4"></li>
+				<li id="theme5"></li>
+				<li id="theme6"></li>
+			  </ul>
+               <hr>
+			  <p class="mb-0">Gradient Background</p>
+			  <hr>
+			  
+			  <ul class="switcher">
+				<li id="theme7"></li>
+				<li id="theme8"></li>
+				<li id="theme9"></li>
+				<li id="theme10"></li>
+				<li id="theme11"></li>
+				<li id="theme12"></li>
+				<li id="theme13"></li>
+				<li id="theme14"></li>
+				<li id="theme15"></li>
+			  </ul>
+		</div>
+	</div>
+	<!--end switcher-->
+	<!-- Bootstrap JS -->
+	<script src="<?= urlOf('assets/js/bootstrap.bundle.min.js') ?>"></script>
+	<!--plugins-->
+	<script src="<?= urlOf('assets/js/jquery.min.js') ?>"></script>
+	<!--Password show & hide js -->
+	<script>
+		$(document).ready(function () {
+			$("#show_hide_password a").on('click', function (event) {
+				event.preventDefault();
+				if ($('#show_hide_password input').attr("type") == "text") {
+					$('#show_hide_password input').attr('type', 'password');
+					$('#show_hide_password i').addClass("bx-hide");
+					$('#show_hide_password i').removeClass("bx-show");
+				} else if ($('#show_hide_password input').attr("type") == "password") {
+					$('#show_hide_password input').attr('type', 'text');
+					$('#show_hide_password i').removeClass("bx-hide");
+					$('#show_hide_password i').addClass("bx-show");
+				}
+			});
+		});
+	</script>
+	
+	
+	<script>
+	$(".switcher-btn").on("click", function() {
+		$(".switcher-wrapper").toggleClass("switcher-toggled")
+	}), $(".close-switcher").on("click", function() {
+		$(".switcher-wrapper").removeClass("switcher-toggled")
+	}),
+
+
+	$('#theme1').click(theme1);
+    $('#theme2').click(theme2);
+    $('#theme3').click(theme3);
+    $('#theme4').click(theme4);
+    $('#theme5').click(theme5);
+    $('#theme6').click(theme6);
+    $('#theme7').click(theme7);
+    $('#theme8').click(theme8);
+    $('#theme9').click(theme9);
+    $('#theme10').click(theme10);
+    $('#theme11').click(theme11);
+    $('#theme12').click(theme12);
+    $('#theme13').click(theme13);
+    $('#theme14').click(theme14);
+    $('#theme15').click(theme15);
+
+    function theme1() {
+      $('body').attr('class', 'bg-theme bg-theme1');
+    }
+
+    function theme2() {
+      $('body').attr('class', 'bg-theme bg-theme2');
+    }
+
+    function theme3() {
+      $('body').attr('class', 'bg-theme bg-theme3');
+    }
+
+    function theme4() {
+      $('body').attr('class', 'bg-theme bg-theme4');
+    }
+	
+	function theme5() {
+      $('body').attr('class', 'bg-theme bg-theme5');
+    }
+	
+	function theme6() {
+      $('body').attr('class', 'bg-theme bg-theme6');
+    }
+
+    function theme7() {
+      $('body').attr('class', 'bg-theme bg-theme7');
+    }
+
+    function theme8() {
+      $('body').attr('class', 'bg-theme bg-theme8');
+    }
+
+    function theme9() {
+      $('body').attr('class', 'bg-theme bg-theme9');
+    }
+
+    function theme10() {
+      $('body').attr('class', 'bg-theme bg-theme10');
+    }
+
+    function theme11() {
+      $('body').attr('class', 'bg-theme bg-theme11');
+    }
+
+    function theme12() {
+      $('body').attr('class', 'bg-theme bg-theme12');
+    }
+
+	function theme13() {
+		$('body').attr('class', 'bg-theme bg-theme13');
+	  }
+	  
+	  function theme14() {
+		$('body').attr('class', 'bg-theme bg-theme14');
+	  }
+	  
+	  function theme15() {
+		$('body').attr('class', 'bg-theme bg-theme15');
+	  }
+
+	</script>
+
+
+
+<script>
+	function sendData() {
+		var data = {
+            username: $('#username').val(),
+            password: $('#password').val()
+        }
+
+        $.post('../../api/login.php', data, function (response) {
+            console.log(response);
+            if (response.success !== true)
+                return;
+
+			window.alert("Login Succesfully.......");
+			window.location.href='../../';        
+		});
+    }
+</script>
+
+	</body>
+	
+	
+	<!-- Mirrored from codervent.com/dashtrans/demo/horizontal/auth-basic-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 11 Apr 2024 10:15:31 GMT -->
+	</html>
