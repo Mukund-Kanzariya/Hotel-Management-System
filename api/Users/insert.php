600ebb8eb3ab211abe03e446e6d6d4cff458b2cf
Mukund-Kanzariya <mukundkanzariya4@gmail.com>
2024-05-11 10:00:42 +0530

    Users api done

--- api/Users/insert.php
+++ api/Users/insert.php
@@ -4,6 +4,7 @@ require('../../includes/init.php');
 
 $roleid=$_POST['roleid'];
 $name=$_POST['name'];
+$password=$_POST['password'];
 $salary=$_POST['salary'];
 $email=$_POST['email'];
 $mobile=$_POST['mobile'];
@@ -11,8 +12,8 @@ $address=$_POST['address'];
 $city=$_POST['city'];
 $state=$_POST['state'];
 
-$query="INSERT INTO `users`(RoleId,Name,Salary,Email,Mobile,Address,City,State) VALUES(?,?,?,?,?,?,?,?)";
-$param=[$roleid,$name,$salary,$email,$mobile,$address,$city,$state];
+$query="INSERT INTO `users`(RoleId,Name,Password,Salary,Email,Mobile,Address,City,State) VALUES(?,?,?,?,?,?,?,?,?)";
+$param=[$roleid,$name,$password,$salary,$email,$mobile,$address,$city,$state];
 execute($query,$param);
 
 ?>
